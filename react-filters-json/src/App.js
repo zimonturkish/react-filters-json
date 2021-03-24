19bc53e2108ceacf7d08ef3296917df7801be509
zimonturkish <simoturco85@gmail.com>
2021-03-24 01:52:47 +0100

    displaying list of movies better in App w Cards

--- react-filters-json/src/App.js
+++ react-filters-json/src/App.js
@@ -1,41 +1,32 @@
 import React from "react";
 import listMovies from "./list-movies.json";
 import { Card } from "react-bootstrap";
-import "./App.css"
+import "./App.css";
 
 class App extends React.Component {
   render() {
-    console.log(listMovies.results);
     return (
       <div>
-        {listMovies.results.map((film) => {
+        {listMovies.results.map((film, index) => {
           return (
-            // style={{ alignItems: "center", backgroundColor: "black" }}
-            <div class="style-card">
+            <div className="style-card" key={index}>
               <Card style={{ width: "28rem" }}>
                 <Card.Img variant="top" src={film.backdrop_path} />
                 <Card.Body>
                   <Card.Title>{film.title}</Card.Title>
                   <hr></hr>
                   <Card.Text>
-                    <p>
-                      <b>Original title</b>: {film.original_title}
-                    </p>
-                    <p>
-                      <b>Original language</b>: {film.original_language}
-                    </p>
-                    <p>
-                      <b>Release Date</b>: {film.release_date}
-                    </p>
-                    <p>
-                      <b>Vote average</b>: {film.vote_average}
-                    </p>
-                    <p>
-                      <b>Vote counts</b>: {film.vote_count}
-                    </p>
-                    <p>
-                      <b>Overview</b>: {film.overview}
-                    </p>
+                    <b>Original title</b>: {film.original_title}
+                    <br />
+                    <b>Original language</b>: {film.original_language}
+                    <br />
+                    <b>Release Date</b>: {film.release_date}
+                    <br />
+                    <b>Vote average</b>: {film.vote_average}
+                    <br />
+                    <b>Vote counts</b>: {film.vote_count}
+                    <br />
+                    <b>Overview</b>: {film.overview}
                   </Card.Text>
                 </Card.Body>
               </Card>
